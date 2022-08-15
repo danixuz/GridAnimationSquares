9d3376e1001d3746e245b587a0b8e8ed18330b4d
danixuz <danixuz@icloud.com>
2022-08-15 03:20:03 +0300

    Initial Commit

---
+++ GridAnimation/GridAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridAnimationApp.swift
+//  GridAnimation
+//
+//  Created by Daniel Spalek on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GridAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
