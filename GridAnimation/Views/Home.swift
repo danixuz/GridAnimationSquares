31fda476e70b922c92bd7ae712bb7ef42d3e47a8
danixuz <danixuz@icloud.com>
2022-08-16 03:54:18 +0300

    updated animation & added colors

--- GridAnimation/Views/Home.swift
+++ GridAnimation/Views/Home.swift
@@ -22,19 +22,30 @@ struct Home: View {
             // Multiplying each row count
             let itemCount = Int((size.height / width).rounded()) * 10 // how many squares can we fit in each column
             
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 10), spacing: 0) {
-                ForEach(0..<itemCount, id: \.self){ _ in
-                    GeometryReader { innerProxy in
-                        let rect = innerProxy.frame(in: .named("GESTURE"))
-                        let scale = itemScale(rect: rect, size: size)
-                        RoundedRectangle(cornerRadius: 4)
-                            .fill(Color.orange)
-                            .scaleEffect(scale)
+            // MARK: For solid linear gradient , we are going to use mask.
+            LinearGradient(colors: [
+                .cyan,
+                .yellow,
+                .mint,
+                .pink,
+                .purple
+            ], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .mask {
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 0), count: 10), spacing: 0) {
+                    ForEach(0..<itemCount, id: \.self){ _ in
+                        GeometryReader { innerProxy in
+                            let rect = innerProxy.frame(in: .named("GESTURE"))
+                            let scale = itemScale(rect: rect, size: size)
+                            RoundedRectangle(cornerRadius: 4)
+                                .fill(Color.orange)
+                                .scaleEffect(scale)
+                        }
+                        .padding(5)
+                        .frame(height: width)
                     }
-                    .padding(5)
-                    .frame(height: width)
                 }
             }
+            
         }
         .padding(15)
         .gesture(
@@ -56,8 +67,10 @@ struct Home: View {
         let root = sqrt((a * a) + (b * b))
         let diagonalValue = sqrt((size.width * size.width) + (size.height * size.height))
         
-        let scale = root / diagonalValue
+        let modifier: CGFloat = 2.0
+        let scale = root / (diagonalValue / modifier)
         let modifiedScale = location == .zero ? 1 : (1 - scale) // if finger is not touching then we don't want it to be active.
+        
         return modifiedScale > 0 ? modifiedScale : 0.001
     }
     
